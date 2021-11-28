8078b6eacbbc302cbe06f42e4a5350fe0e9cfe1e
Alexander Chai <alexanderchai416@gmail.com>
2021-11-28 19:25:02 +0800

    Complete registration

--- app/screens/register.js
+++ app/screens/register.js
@@ -8,28 +8,82 @@ import {
   Button,
 } from 'react-native-elements'
 import externalStyle from '../style/style'
+import axios from 'axios';
+import constants from '../modules/constants';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
+import globals from '../modules/globals.js';
 
 export default class Register extends React.Component {
   state = {
-    username: '', password: '', email: '', phone_number: ''
+    email: '', username: '', password: '', password1: ''
   }
   onChangeText = (key, val) => {
     this.setState({ [key]: val })
   }
   signUp = async () => {
-    const { username, password, email, phone_number } = this.state
-    // try {
-    //   // here place your signup logic
-    //   console.log('user successfully signed up!: ', success)
-    // } catch (err) {
-    //   console.log('error signing up: ', err)
-    // }
-    navigation.navigate('home')
+    const { 
+      email, 
+      username, 
+      password, 
+      password1,
+    } = this.state
+    if (email && username && password && password1){
+      await axios({
+        method: 'post',
+        url: constants.URL+'/api/account/register',
+        data: {
+          email: email,
+          username: username,
+          password: password,
+          password1: password1,
+        }
+      }).then(function (response) {
+        console.log(response.data);
+        if (response.data.token) {
+          Toast.show({
+            type: 'success',
+            position: 'bottom',
+            text1: "Account Created!",
+          })
+          globals.setLogin();
+        }
+        else if (response.data.email || response.data.username) {
+          Toast.show({
+            type: 'error',
+            position: 'bottom',
+            text1: response.data.email? response.data.email : response.data.username,
+          })
+        }
+      }).catch((error) => {
+        console.log(error.response.data.password);
+        Toast.show({
+          type: 'error',
+          position: 'bottom',
+          text1: error.response.data.password,
+        })
+      })
+    }
+    // check if fields are empty
+    else {
+      Toast.show({
+        type: 'error',
+        position: 'bottom',
+        text1: 'Please insert all the fields',
+      })
+    }
   }
  
   render() {
     return (
       <View style={externalStyle.container}>
+        <TextInput
+          style={externalStyle.input}
+          placeholder='Email'
+          autoCapitalize="none"
+          placeholderTextColor='grey'
+          keyboardType='email-address'
+          onChangeText={val => this.onChangeText('email', val)}
+        />
         <TextInput
           style={externalStyle.input}
           placeholder='Username'
@@ -47,25 +101,19 @@ export default class Register extends React.Component {
         />
         <TextInput
           style={externalStyle.input}
-          placeholder='Email'
+          placeholder='Re-enter password'
+          secureTextEntry={true}
           autoCapitalize="none"
           placeholderTextColor='grey'
-          keyboardType='email-address'
-          onChangeText={val => this.onChangeText('email', val)}
-        />
-        <TextInput
-          style={externalStyle.input}
-          placeholder='Phone Number'
-          autoCapitalize="none"
-          placeholderTextColor='grey'
-          keyboardType="phone-pad"
-          onChangeText={val => this.onChangeText('phone_number', val)}
+          onChangeText={val => this.onChangeText('password1', val)}
         />
         <Button
           buttonStyle={externalStyle.button}
           type="solid"
           title='Sign Up'
-          onPress={this.signUp}
+          onPress={ 
+            this.signUp
+          }
         />
       </View>
     )
