639453fbd4e2cf7b4b2b9bf2440786c0a1c95517
Alexander Chai <alexanderchai416@gmail.com>
2021-11-30 21:11:27 +0800

    complete logout

--- app/screens/profile.js
+++ app/screens/profile.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-
+import { 
+  Text,
+  View, 
+} from 'react-native';
+import { Button } from 'react-native-elements';
+import externalStyle from '../style/style';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import globals from '../modules/globals'
 export default class Profile extends Component {
 componentWillUnmount(){
     console.log("Profile");
 }
+logout = async() => {
+ try{
+  //  const token = AsyncStorage.removeItem('token')
+  const token = AsyncStorage.clear()
+  console.log('token removed');
+  globals.setLogout();
+   
+ }catch(e){
+   console.log(e)
+ }
+}
   render() {
     return (
-      <Text>Profile page</Text>
+      <View style = {externalStyle.container}>
+        <Button
+          buttonStyle={externalStyle.button}
+          type="solid"
+          title="Logout"     
+          onPress={this.logout}    
+        />
+      </View>
+
     );
   }
 }
