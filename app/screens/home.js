f61e294f50efdfb64da59c1eb6cb4762cbd7be3f
Alexander Chai <alexanderchai416@gmail.com>
2022-01-04 01:26:38 +0800

    Added product filtering

--- app/screens/home.js
+++ app/screens/home.js
@@ -9,6 +9,7 @@ import RNPickerSelect from 'react-native-picker-select';
 import axios from 'axios';
 import constants from '../modules/constants.js';
 import Item from '../modules/product_item.js';
+import ProductList from '../modules/product_render.js';
 
 export default class Home extends Component {
   constructor(props) {
@@ -20,15 +21,15 @@ export default class Home extends Component {
       }
   }
   // Render Product Item
-  renderItem = ({ item }) => (
-    <Item 
-      id={item.id}
-      image={item.image}
-      name={item.name}
-      price={item.price}
-      // onPress={() => setSelectedId(item.id)}
-    />
-  );
+  // renderItem = ({ item }) => (
+  //   <Item 
+  //     id={item.id}
+  //     image={item.image}
+  //     name={item.name}
+  //     price={item.price}
+  //     // onPress={() => setSelectedId(item.id)}
+  //   />
+  // );
  componentWillUnmount(){
     console.log("Will Unmount");
  }
@@ -56,8 +57,8 @@ export default class Home extends Component {
           category : category
         })
     })
-  } catch (e){
-    console.log(e);
+  } catch (error){
+    console.log(error);
   }
   // GET all product request
   try{
@@ -68,16 +69,42 @@ export default class Home extends Component {
       this.setState({products: response.data});
       console.log(this.state.products)
     })
-  } catch (e){
-    console.log(e);
+  } catch (error){
+    console.log(error);
   }
 }
 updateSearch = (search) => {
   this.setState({ search });
 };
 
-searchCategory = (value) => {
-  console.log(value)
+searchCategory = async(value) => {
+  if (value == 'all'){
+    try{
+      await axios({
+        method: 'get',
+        url: constants.URL+'/api/product'
+      }).then((response) => {
+        this.setState({products: response.data});
+        console.log(this.state.products)
+      })
+    } catch (error){
+      console.log(error);
+    }
+  }
+  else{
+    try{
+      await axios({
+        method: 'get',
+        url: constants.URL+'/api/product/?category='+value
+      }).then((response) => {
+        this.setState({products: response.data});
+        console.log(this.state.products)
+      })
+    } catch (error){
+      console.log(error.response.data)
+    }
+  }
+  console.log(value);
 }
   render() {
     const { search } = this.state;
@@ -116,7 +143,10 @@ searchCategory = (value) => {
             }}
             onValueChange={(value) => this.searchCategory(value)}
             placeholder={
-              {label: 'Filter'}
+              {
+                label: 'All',
+                value: 'all',
+              }
             }
             // Populate category in items
             items = {this.state.category}
@@ -124,18 +154,24 @@ searchCategory = (value) => {
         </View>
       );
     };
-    return (
-      <SafeAreaView style={{flex: 1 }}>
-        <FlatList
-          style = {styles.productItem}
-          data={this.state.products}
-          renderItem={item => this.renderItem(item)}
-          horizontal={false}
-          numColumns={2}
-          ListHeaderComponent={getHeader}
-        />
-      </SafeAreaView>  
-    );
+    // return (
+    //   <SafeAreaView style={{flex: 1 }}>
+    //     <FlatList
+    //       style = {styles.productItem}
+    //       data={this.state.products}
+    //       renderItem={item => this.renderItem(item)}
+    //       horizontal={false}
+    //       numColumns={2}
+    //       ListHeaderComponent={getHeader}
+    //     />
+    //   </SafeAreaView>  
+    // );
+    return(
+      <ProductList 
+        products={this.state.products}
+        header={getHeader()}
+      />
+    )
   }
 }
 
