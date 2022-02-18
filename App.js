2895dfc9b6138e62771c395206b3814f84cbc0db
Alexander Chai <alexanderchai416@gmail.com>
2022-02-18 18:27:28 +0800

    Check if logout

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './app/screens/home.js';
@@ -9,13 +9,17 @@ import Notification from './app/screens/notification.js';
 import Cart from './app/screens/cart.js';
 import Register from './app/screens/register.js';
 import Product from './app/screens/single_product.js';
-import Checkout from './app/screens/checkout.js';
+import PaymentOption from './app/screens/payment_option.js';
+import DeliveryOption from './app/screens/delivery_option.js';
+import Order from './app/screens/order.js';
+import Payment from './app/screens/payment.js';
 import { renderNode } from 'react-native-elements/dist/helpers';
 import globals from './app/modules/globals.js';
 import { Text } from 'react-native';
 import Profile from './app/screens/profile.js';
 import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { StackActions, NavigationActions } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -63,7 +67,6 @@ export default class App extends Component {
             })
         }
     }
-
     render() {
         return (
             <NavigationContainer>
@@ -76,7 +79,7 @@ export default class App extends Component {
                                 iconName = 'home'
                             } else if (route.name === 'Notification') {
                                 iconName = 'bell'
-                            } else if (route.name === 'CartStack') {
+                            } else if (route.name === 'CartStack' || route.name === "Cart") {
                                 iconName = 'shopping-cart'
                             } else if (route.name === 'LoginStack' || route.name === 'ProfileStack') {
                                 iconName = 'user'
@@ -95,7 +98,11 @@ export default class App extends Component {
                     {/* <Tab.Screen name="Home" component={Home}  /> */}
                     <Tab.Screen name="HomeStack" component={HomeStack} options={{ title: "Home", headerShown: false }} />
                     <Tab.Screen name="Notification" component={Notification} />
-                    <Tab.Screen name="CartStack" component={CartStack} options={{title: "Cart", headerShown: false}} />
+                    {
+                        !this.state.isLogin ?
+                            <Tab.Screen name="Cart" component={Cart}/> :
+                            <Tab.Screen name="CartStack" component={CartStack} options={{title: "Cart", headerShown: false}} />
+                    }
                     {
                         !this.state.isLogin ?
                             <Tab.Screen name="LoginStack" component={LoginStack} options={{ title: "Login", headerShown: false }} /> :
@@ -129,7 +136,10 @@ function CartStack() {
     return(
         <Stack.Navigator>
             <Stack.Screen name="Cart" component={Cart}/>
-            <Stack.Screen name="Checkout" component={Checkout} />
+            <Stack.Screen name="PaymentOption" component={PaymentOption} options={{title: 'Confirm Checkout'}}/>
+            <Stack.Screen name="DeliveryOption" component={DeliveryOption} options={{title: 'Delivery Option'}}/>
+            <Stack.Screen name="Payment" component={Payment} options={{title: 'Payment Details'}}/>
+            <Stack.Screen name="Order" component={Order} options={{title: 'Order'}} />
         </Stack.Navigator>
     )
 }
